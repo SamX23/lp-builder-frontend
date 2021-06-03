a7594f73e3b828a1cd3d73074b65dde06b4e09eb
samx23 <skalexsong@gmail.com>
2021-06-03 23:20:33 +0700

    Update : change blocks section positions

--- src/component/EditorPanel.js
+++ src/component/EditorPanel.js
@@ -1,5 +1,6 @@
 const EditorPanel = () => (
   <div className="panel__right">
+    <div className="blocks-container"></div>
     <div className="layers-container"></div>
     <div className="styles-container"></div>
     <div className="traits-container"></div>
