7b54a1ed615f40bd9b4709e7c54d0437ea732b39
samx23 <skalexsong@gmail.com>
2021-06-03 22:11:21 +0700

    Update : remove comments

--- src/component/ContentPanel.js
+++ src/component/ContentPanel.js
@@ -2,11 +2,9 @@ import EditorPanel from "./EditorPanel";
 
 const ContentPanel = () => (
   <div className="editor-row">
-    {/* CONTENT SECTION */}
     <div className="editor-canvas">
       <div id="gjs"></div>
     </div>
-    {/* EDITOR PANEL SECTION */}
     <EditorPanel />
   </div>
 );
