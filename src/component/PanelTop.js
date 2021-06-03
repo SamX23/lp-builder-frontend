3e27cfda85a329b70816254336d4dbdd902925b9
samx23 <skalexsong@gmail.com>
2021-06-03 21:15:55 +0700

    Create : add top panel

---
+++ src/component/PanelTop.js
@@ -0,0 +1,9 @@
+const PanelTop = () => (
+  <div className="panel__top">
+    <div className="panel__basic-actions"></div>
+    <div className="panel__devices"></div>
+    <div className="panel__switcher"></div>
+  </div>
+);
+
+export default PanelTop;
