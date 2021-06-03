0f781a5d465eddf9079e98e74d127fcf68888297
samx23 <skalexsong@gmail.com>
2021-06-03 22:11:12 +0700

    Update : add logo

--- src/component/PanelTop.js
+++ src/component/PanelTop.js
@@ -1,5 +1,11 @@
 const PanelTop = () => (
   <div className="panel__top">
+    <div className="lp_logo">
+      <img
+        src="https://a9x6f5q3.rocketcdn.me/wp-content/uploads/2020/02/logo-ke-header.png"
+        alt="logo"
+      />
+    </div>
     <div className="panel__basic-actions"></div>
     <div className="panel__devices"></div>
     <div className="panel__switcher"></div>
