f8ebf38ae153bee4a698f5bfbb8fba502f0d227a
samx23 <skalexsong@gmail.com>
2021-06-03 21:15:47 +0700

    Create : add editor panel

---
+++ src/component/EditorPanel.js
@@ -0,0 +1,9 @@
+const EditorPanel = () => (
+  <div className="panel__right">
+    <div className="layers-container"></div>
+    <div className="styles-container"></div>
+    <div className="traits-container"></div>
+  </div>
+);
+
+export default EditorPanel;
