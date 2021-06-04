97ea487b87a8d07bfc4e98648d1ce80d1bdda0cb
samx23 <skalexsong@gmail.com>
2021-06-04 22:17:43 +0700

    Update : refactor logo classname

--- src/component/PanelTop.js
+++ src/component/PanelTop.js
@@ -1,6 +1,6 @@
 const PanelTop = () => (
   <div className="panel__top">
-    <div className="lp_logo">
+    <div className="panel__logo">
       <img
         src="https://a9x6f5q3.rocketcdn.me/wp-content/uploads/2020/02/logo-ke-header.png"
         alt="logo"
