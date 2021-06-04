891085685c6a7ec4d5ca612a2b150fe34b7d2127
samx23 <skalexsong@gmail.com>
2021-06-04 23:36:01 +0700

    Update : change id naming

--- src/component/ContentPanel.js
+++ src/component/ContentPanel.js
@@ -3,7 +3,7 @@ import EditorPanel from "./EditorPanel";
 const ContentPanel = () => (
   <div className="editor-row">
     <div className="editor-canvas">
-      <div id="gjs"></div>
+      <div id="builder-canvas"></div>
       <div className="editor-canvas-mode">
         <div className="panel__devices"></div>
       </div>
